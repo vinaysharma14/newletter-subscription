3095f1124c5270b616cf42dbed8794d9642a216a
Vinay Sharma <vinaysharma7811@gmail.com>
2021-03-14 00:42:41 +0530

    feature: API designed to subscribe to newsletter

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,7 @@ export const configureDb = async (db: string, host: string, port: string) => {
   const url = `mongodb://${host}:${port}/${db}`;
 
   try {
-    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
   } catch (error) {
     throw new Error(`unable to connect to database: ${error}`);
   }
--- server/src/controllers/newsletter.ts
+++ server/src/controllers/newsletter.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-const subscribe = (req: Request, res: Response) => {
+import { subscriptions } from '../schema';
+
+const subscribe = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  try {
+    const subscription = new subscriptions({ email });
+    await subscription.save();
+  } catch ({ code, message }) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ code, message });
+  }
+
   res.sendStatus(StatusCodes.OK);
 };
 
